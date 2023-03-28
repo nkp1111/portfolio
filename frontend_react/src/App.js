3e448eb3befeb6dd37dc113861db09c9b3c08664
nkp1111 <neerajp4321@gmail.com>
2023-03-28 12:20:32 +0530

    refactor: change folder name and import containers

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
 const App = () => {
   return (
-    <div>
-      App
+    <div className='app'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   )
 }
