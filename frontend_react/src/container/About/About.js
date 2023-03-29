7947bc64d09b6222387f5fbbf40a61947f99a92e
nkp1111 <neerajp4321@gmail.com>
2023-03-29 11:03:06 +0530

    feat: add about component with its styles

--- frontend_react/src/container/About/About.js
+++ frontend_react/src/container/About/About.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
 
+import { images } from '../../constants'
 import "./About.scss"
+import { client, urlFor } from '../../client'
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = `*[ _type == "abouts" ]`
+    client.fetch(query)
+      .then((data) => {
+        setAbouts(data)
+      })
+  }, []);
+
   return (
-    <div>
-      About
-    </div>
+    <>
+      <h2 className="head-text">
+        I Know That
+        <span> Good Development</span>
+        <br />
+        means
+        <span> Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((item, ind) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={item.title + ind}
+          >
+            <img src={urlFor(item.imgUrl)} alt={item.title} />
+            <h2 className="bold-text"
+              style={{ marginTop: 20 }}
+            >{item.title}</h2>
+            <p className="p-text"
+              style={{ marginTop: 20 }}
+            >{item.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
